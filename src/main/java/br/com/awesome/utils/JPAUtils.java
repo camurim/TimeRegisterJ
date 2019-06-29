a494e259e6db137596ab2aec8ff344b6bd23bfd3
Carlos Amorim <caamorimcoelho@gmail.com>
2019-06-29 08:02:51 -0300

    Mais correções no projeto

---
+++ src/main/java/br/com/awesome/utils/JPAUtils.java
@@ -0,0 +1,15 @@
+package br.com.awesome.utils;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletRequest;
+
+public class JPAUtils {
+	public static EntityManager JpaEntityManager() {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = facesContext.getExternalContext();
+		HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
+		return (EntityManager) request.getAttribute("entityManager");
+	}
+}
