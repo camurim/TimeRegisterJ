af98a1c9c70bb98ff5161d5cf52a8159b7ddd304
carlos <carlos@NDS01>
2019-07-01 16:39:46 -0300

    Correções no GenericController

--- src/main/java/br/com/awesome/controller/generic/GenericController.java
+++ src/main/java/br/com/awesome/controller/generic/GenericController.java
@@ -2,9 +2,11 @@ package br.com.awesome.controller.generic;
 
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.inject.spi.CDI;
+import javax.persistence.TransactionRequiredException;
 
 import br.com.awesome.model.generic.AbstractModel;
 import br.com.awesome.repository.generic.GenericRepository;
@@ -20,22 +22,73 @@ public class GenericController <T extends AbstractModel<T>, Y extends GenericRep
 	protected List<T> suggestions;
 	protected boolean fetchAll;
 	protected String orderBy = null;
-	private final Class<T> persistenceClass;
+	private final Class<T> modelClass;
 	private GenericRepository<T, Long> repository;
 	
 	@SuppressWarnings("unchecked")
-	public GenericController() {
-		this.persistenceClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass())
+	public GenericController() throws ClassNotFoundException {
+		injectRepository();
+		this.modelClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass())
 				.getActualTypeArguments()[0];
 		try {
-			this.model = (T) (Class.forName(this.persistenceClass.getCanonicalName()).newInstance());
-			this.filter = (T) (Class.forName(this.persistenceClass.getCanonicalName()).newInstance());
+			this.model = (T) (Class.forName(this.modelClass.getCanonicalName()).newInstance());
+			this.filter = (T) (Class.forName(this.modelClass.getCanonicalName()).newInstance());
 		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public void injectRepository() throws ClassNotFoundException {
+	public String save() {
+		try {
+			this.repository.saveOrUpdate(this.model);
+		} catch (IllegalArgumentException | TransactionRequiredException e) {
+		}
+		return "";
+	}
+	
+	public void delete() {
+		try {
+			this.repository.delete(model);
+		} catch (IllegalArgumentException | TransactionRequiredException e) {
+		}
+	}
+	
+	public List<T> getListData() {
+		if (this.listData == null) {
+			this.listData = new ArrayList<T>();
+		}
+
+		if (this.listData.size() == 0 && this.fetchAll) {
+			this.listData = repository.fetchAll(this.orderBy);
+		}
+
+		return this.listData;
+	}
+	
+	public T getModel() {
+		return this.model;
+	}
+	
+	public void setModel(T model) {
+		this.model = model;
+	}
+	
+	public void setFilter(T filter) {
+		this.filter = filter;
+	}
+	
+	public T getFilter() {
+		return this.filter;
+	}
+		
+	public void clear() throws InstantiationException, IllegalAccessException {
+		listData = new ArrayList<T>();
+		model = this.modelClass.newInstance();
+		filter = this.modelClass.newInstance();
+		fetchAll = true;
+	}
+	
+	private void injectRepository() throws ClassNotFoundException {
 		Class<?> cl = this.getClass();
 		if (cl.isAnnotationPresent(JpaRepository.class)) {
 			JpaRepository jpaRepository = cl.getAnnotation(JpaRepository.class);
